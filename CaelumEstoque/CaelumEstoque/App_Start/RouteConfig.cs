9488b391edb7f01efc697b58633f3a1c6a7b3a6b
felipepatente <felipepatente@yahoo.com.br>
2019-01-10 17:56:17 -0200

    Criando Rotas na Aplicação

--- CaelumEstoque/CaelumEstoque/App_Start/RouteConfig.cs
+++ CaelumEstoque/CaelumEstoque/App_Start/RouteConfig.cs
@@ -11,12 +11,12 @@ namespace CaelumEstoque
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapMvcAttributeRoutes();
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
